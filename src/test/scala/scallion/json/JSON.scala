b92531da764b5fa43299868e034dd2650bfaa0f4
Romain Edelmann <romain.edelmann@gmail.com>
2019-07-16 16:05:41 +0200

    Use JSON example in tests

---
+++ src/test/scala/scallion/json/JSON.scala
@@ -0,0 +1,165 @@
+/* Copyright 2019 EPFL, Lausanne
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package scallion.json
+
+import scala.language.implicitConversions
+
+import scallion.input._
+import scallion.lexing._
+import scallion.parsing._
+import scallion.parsing.visualization._
+
+sealed abstract class Token {
+  val range: (Int, Int)
+}
+case class SeparatorToken(value: Char, range: (Int, Int)) extends Token
+case class BooleanToken(value: Boolean, range: (Int, Int)) extends Token
+case class NumberToken(value: Double, range: (Int, Int)) extends Token
+case class StringToken(value: String, range: (Int, Int)) extends Token
+case class NullToken(range: (Int, Int)) extends Token
+case class SpaceToken(range: (Int, Int)) extends Token
+case class UnknownToken(content: String, range: (Int, Int)) extends Token
+
+sealed abstract class TokenClass(repr: String) {
+  override def toString = repr
+}
+case class SeparatorClass(value: Char) extends TokenClass(value.toString)
+case object BooleanClass extends TokenClass("<boolean>")
+case object NumberClass extends TokenClass("<number>")
+case object StringClass extends TokenClass("<string>")
+case object NullClass extends TokenClass("<null>")
+case object NoClass extends TokenClass("<error>")
+
+sealed abstract class Value {
+  val range: (Int, Int)
+}
+case class ArrayValue(elems: Seq[Value], range: (Int, Int)) extends Value
+case class ObjectValue(elems: Seq[(StringValue, Value)], range: (Int, Int)) extends Value
+case class BooleanValue(value: Boolean, range: (Int, Int)) extends Value
+case class NumberValue(value: Double, range: (Int, Int)) extends Value
+case class StringValue(value: String, range: (Int, Int)) extends Value
+case class NullValue(range: (Int, Int)) extends Value
+
+object JSONLexer extends Lexers[Token, Char, Int] with CharRegExps {
+
+  val lexer = Lexer(
+    // Separator
+    oneOf("[]{},:")
+      |> { (cs, r) => SeparatorToken(cs.head, r) },
+
+    // Space
+    many1(whiteSpace)
+      |> { (_, r) => SpaceToken(r) },
+
+    // Booleans
+    word("true")
+      |> { (_, r) => BooleanToken(true, r) },
+    word("false")
+      |> { (_, r) => BooleanToken(false, r) },
+
+    // Null
+    word("null")
+      |> { (_, r) => NullToken(r) },
+
+    // Strings
+    elem('"') ~
+    many {
+      elem(c => c != '"' && c != '\\' && !c.isControl) |
+      elem('\\') ~ (oneOf("\"\\/bfnrt") | elem('u') ~ hex.times(4))
+    } ~
+    elem('"')
+      |> { (cs, r) => {
+        val string = cs.mkString
+        StringToken(string.slice(1, string.length - 1), r)
+      }},
+
+    // Numbers
+    opt {
+      elem('-')
+    } ~
+    {
+      elem('0') |
+      nonZero ~ many(digit)
+    } ~
+    opt {
+      elem('.') ~ many1(digit)
+    } ~
+    opt {
+      oneOf("eE") ~
+      opt(oneOf("+-")) ~
+      many1(digit)
+    }
+      |> { (cs, r) => NumberToken(cs.mkString.toDouble, r) }
+  ) onError {
+    (cs, r) => UnknownToken(cs.mkString, r)
+  }
+
+  def apply(it: Iterator[Char]): Iterator[Token] = {
+    val source = Source.fromIterator(it, IndexPositioner)
+
+    val tokens = lexer.spawn(source)
+
+    tokens.filter(!_.isInstanceOf[SpaceToken])
+  }
+}
+
+object JSONParser extends Parsers[Token, TokenClass] {
+
+  override def getKind(token: Token): TokenClass = token match {
+    case SeparatorToken(value, _) => SeparatorClass(value)
+    case BooleanToken(_, _) => BooleanClass
+    case NumberToken(_, _) => NumberClass
+    case StringToken(_, _) => StringClass
+    case NullToken(_) => NullClass
+    case _ => NoClass
+  }
+
+  val booleanValue = accept(BooleanClass) {
+    case BooleanToken(value, range) => BooleanValue(value, range)
+  }
+  val numberValue = accept(NumberClass) {
+    case NumberToken(value, range) => NumberValue(value, range)
+  }
+  val stringValue = accept(StringClass) {
+    case StringToken(value, range) => StringValue(value, range)
+  }
+  val nullValue = accept(NullClass) {
+    case NullToken(range) => NullValue(range)
+  }
+  implicit def separator(char: Char) = accept(SeparatorClass(char)) {
+    case SeparatorToken(_, range) => range
+  }
+
+  lazy val arrayValue =
+    ('[' ~ repsep(value, ',') ~ ']').map {
+      case start ~ vs ~ end => ArrayValue(vs, (start._1, end._2))
+    }
+
+  lazy val binding =
+    (stringValue ~ ':' ~ value).map {
+      case key ~ _ ~ value => (key, value)
+    }
+  lazy val objectValue =
+    ('{' ~ repsep(binding, ',') ~ '}').map {
+      case start ~ bs ~ end => ObjectValue(bs, (start._1, end._2))
+    }
+
+  lazy val value: Parser[Value] = recursive {
+    oneOf(arrayValue, objectValue, booleanValue, numberValue, stringValue, nullValue)
+  }
+
+  def apply(it: Iterator[Token]): ParseResult[Value] = value(it)
+}
---
+++ src/test/scala/scallion/json/JSONParserTests.scala
@@ -0,0 +1,157 @@
+
+package scallion.json
+
+import org.scalatest._
+
+class JSONParserTests extends FlatSpec with Inside {
+
+  def parse(text: String): Option[Value] =
+    JSONParser(JSONLexer(text.toIterator)).getValue
+
+  def rest(text: String): JSONParser.Parser[Value] =
+    JSONParser(JSONLexer(text.toIterator)).parser
+
+  "JSON Parser" should "parse some basic examples" in {
+
+    inside(parse("1.0")) {
+      case Some(NumberValue(value, position)) => {
+        assert(value == 1.0)
+        assert(position == (0, 3))
+      }
+    }
+
+    inside(parse("true")) {
+      case Some(BooleanValue(value, position)) => {
+        assert(value == true)
+        assert(position == (0, 4))
+      }
+    }
+
+    inside(parse("null")) {
+      case Some(NullValue(position)) => {
+        assert(position == (0, 4))
+      }
+    }
+
+    inside(parse("[]")) {
+      case Some(ArrayValue(values, position)) => {
+        assert(values.size == 0)
+        assert(position == (0, 2))
+      }
+    }
+
+    inside(parse("{}")) {
+      case Some(ObjectValue(values, position)) => {
+        assert(values.size == 0)
+        assert(position == (0, 2))
+      }
+    }
+
+    inside(parse("""  "Hello World!" """)) {
+      case Some(StringValue(value, position)) => {
+        assert(value == "Hello World!")
+        assert(position == (2, 16))
+      }
+    }
+
+    inside(parse("""{"hello": null, "hi": "there"}""")) {
+      case Some(ObjectValue(values, position)) => {
+        assert(values.size == 2)
+
+        inside(values(0)) {
+          case (StringValue(key, keyPosition), NullValue(position)) => {
+            assert(key == "hello")
+          }
+        }
+
+        inside(values(1)) {
+          case (StringValue(key, keyPosition), StringValue(value, position)) => {
+            assert(key == "hi")
+            assert(value == "there")
+          }
+        }
+      }
+    }
+
+    inside(parse("""["Hello", null, [42, {}]]""")) {
+      case Some(ArrayValue(values, position)) => {
+        assert(values.size == 3)
+
+        inside(values(0)) {
+          case StringValue(value, position) => {
+            assert(value == "Hello")
+          }
+        }
+
+        inside(values(1)) {
+          case NullValue(position) => ()
+        }
+
+        inside(values(2)) {
+          case ArrayValue(values, position) => {
+            assert(values.size == 2)
+
+            inside(values(0)) {
+              case NumberValue(value, position) => {
+                assert(value == 42)
+              }
+            }
+
+            inside(values(1)) {
+              case ObjectValue(values, position) => {
+                assert(values.isEmpty)
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+
+  "Completions" should "work for arrays" in {
+
+    val completed = JSONParser.arrayValue.complete {
+      case SeparatorClass(sep) => SeparatorToken(sep, (0, 0))
+    }
+
+    assert(completed.nullable == Some(ArrayValue(Seq(), (0, 0))))
+
+  }
+
+  it should "work for objects" in {
+    val completed = JSONParser.objectValue.complete {
+      case SeparatorClass(sep) => SeparatorToken(sep, (0, 0))
+    }
+
+    assert(completed.nullable == Some(ObjectValue(Seq(), (0, 0))))
+  }
+
+  it should "work for general values" in {
+    val completed = JSONParser.value.complete {
+      case NullClass => NullToken((0, 0))
+    }
+
+    assert(completed.nullable == Some(NullValue((0, 0))))
+  }
+
+  it should "work even after errors" in {
+
+    val erroneous = rest("[1,}")
+
+    assert(erroneous.nullable.isEmpty)
+
+    val completed = erroneous.complete {
+      case StringClass => StringToken("XXX", (0, 0))
+      case SeparatorClass(sep) => SeparatorToken(sep, (0, 0))
+    }
+
+    inside(completed.nullable) {
+      case Some(ArrayValue(values, _)) => {
+        assert(values.size == 2)
+        assert(values(0) == NumberValue(1, (1, 2)))
+        assert(values(1) == StringValue("XXX", (0, 0)))
+      }
+    }
+  }
+}
