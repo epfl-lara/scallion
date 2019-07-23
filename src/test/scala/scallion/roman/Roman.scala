b1fb915a22c7a3a6c3173bcbe835b9dff23ea9c9
Romain Edelmann <romain.edelmann@gmail.com>
2019-07-23 11:07:18 +0200

    Add test for Roman numerals

---
+++ src/test/scala/scallion/roman/Roman.scala
@@ -0,0 +1,90 @@
+/* Copyright 2019 EPFL, Lausanne
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package scallion.roman
+
+import scallion.syntactic._
+
+sealed trait Symbol
+case object I extends Symbol
+case object V extends Symbol
+case object X extends Symbol
+case object L extends Symbol
+case object C extends Symbol
+case object D extends Symbol
+case object M extends Symbol
+
+object RomanSyntax extends Syntaxes[Symbol, Symbol] {
+
+  override def getKind(token: Symbol): Symbol = token
+
+  def base(si: Symbol, sv: Symbol, sx: Symbol): Syntax[Int] = {
+
+    val i = elem(si).unit(si)
+    val v = elem(sv).unit(sv)
+    val x = elem(sx).unit(sx)
+
+    epsilon(0) |
+    i ~>~ {
+      epsilon(1) |
+      i ~>~ {
+        epsilon(2) |
+        i ~>~ epsilon(3)
+      } |
+      v ~>~ epsilon(4) |
+      x ~>~ epsilon(9)
+    } |
+    v ~>~ {
+      epsilon(5) |
+      i ~>~ {
+        epsilon(6) |
+        i ~>~ {
+          epsilon(7) |
+          i ~>~ epsilon(8)
+        }
+      }
+    }
+  }
+
+  val units = base(I, V, X)
+  val tens = base(X, L, C)
+  val hundreds = base(C, D, M)
+  val thousands: Syntax[Int] = {
+    val m = elem(M).unit(M)
+
+    epsilon(0) |
+    m ~>~ {
+      epsilon(1) |
+      m ~>~ {
+        epsilon(2) |
+        m ~>~ epsilon(3)
+      }
+    }
+  }
+
+  val number: Syntax[Int] = (thousands ~ hundreds ~ tens ~ units).map({
+    case ths ~ hus ~ tes ~ uns => ths * 1000 + hus * 100 + tes * 10 + uns
+  }, {
+    case n if n >= 0 => {
+      val uns = (n / 1) % 10
+      val tes = (n / 10) % 10
+      val hus = (n / 100) % 10
+      val ths = (n / 1000)
+
+      Seq(ths ~ hus ~ tes ~ uns)
+    }
+    case _ => Seq()
+  })
+}
---
+++ src/test/scala/scallion/roman/RomanSyntaxTests.scala
@@ -0,0 +1,88 @@
+/* Copyright 2019 EPFL, Lausanne
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package scallion.roman
+
+import org.scalatest._
+
+class RomanSyntaxTests extends FlatSpec with Inside {
+
+  import RomanSyntax._
+
+
+  def tokenize(string: String): Iterator[Symbol] =
+    string.map {
+      case 'I' => I
+      case 'V' => V
+      case 'X' => X
+      case 'L' => L
+      case 'C' => C
+      case 'D' => D
+      case 'M' => M
+    }.toIterator
+
+  def display(symbols: Seq[Symbol]): String =
+    symbols.map(_.toString).mkString("")
+
+
+  "Roman syntax" should "be LL(1)" in {
+    assert(number.isLL1)
+  }
+
+  it should "be able to parse various examples" in {
+    assert(number(tokenize("")).getValue == Some(0))
+    assert(number(tokenize("I")).getValue == Some(1))
+    assert(number(tokenize("XII")).getValue == Some(12))
+    assert(number(tokenize("XCIII")).getValue == Some(93))
+    assert(number(tokenize("MCDXLIV")).getValue == Some(1444))
+    assert(number(tokenize("MMMD")).getValue == Some(3500))
+    assert(number(tokenize("XLVI")).getValue == Some(46))
+    assert(number(tokenize("MCDXCI")).getValue == Some(1491))
+    assert(number(tokenize("XVII")).getValue == Some(17))
+    assert(number(tokenize("MDCCCI")).getValue == Some(1801))
+    assert(number(tokenize("MV")).getValue == Some(1005))
+    assert(number(tokenize("CXVIII")).getValue == Some(118))
+    assert(number(tokenize("XXXIX")).getValue == Some(39))
+  }
+
+  it should "be able to pretty print various examples" in {
+    assert(display(number.unapply(232).next()) == "CCXXXII")
+    assert(display(number.unapply(1337).next()) == "MCCCXXXVII")
+    assert(display(number.unapply(14).next()) == "XIV")
+    assert(display(number.unapply(3009).next()) == "MMMIX")
+  }
+
+  "Pretty printing" should "lead to a single value for valid numbers" in {
+    for (i <- 0 until 4000) {
+      val xs = number.unapply(i).toList
+      assert(xs.size == 1)
+    }
+  }
+
+  it should "lead to no values for invalid members" in {
+    assert(!number.unapply(-1).hasNext)
+    assert(!number.unapply(4000).hasNext)
+    assert(!number.unapply(-24241).hasNext)
+    assert(!number.unapply(124214).hasNext)
+  }
+
+  "Parsing" should "be possible on all pretty printed values" in {
+    for (i <- 0 until 4000) {
+      number.unapply(i).map(_.toIterator).foreach { symbols =>
+        assert(number(symbols).getValue == Some(i))
+      }
+    }
+  }
+}
